8f372dede7ecbf34c732129edef52fcc6e4262d2
sandyyj <sndy.julianto@gmail.com>
2022-11-18 13:26:12 +0700

    Add routes.js and update server.js

---
+++ routes.js
@@ -0,0 +1,49 @@
+const { handler } = require("@hapi/hapi/lib/cors")
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage'
+        }
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut'
+        }
+    },
+    {
+        method:'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About Page'
+        }
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut'
+        }
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+           const { name = 'stranger'} = request.params
+           return `Hello, ${name}!` 
+        }
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan'
+        }
+    },
+]
+
+module.exports = routes
