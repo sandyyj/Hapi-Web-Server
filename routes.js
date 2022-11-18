ded583c2a67a7d628d4676a7d73efbb9301f6437
sandyyj <sndy.julianto@gmail.com>
2022-11-18 13:44:28 +0700

    Update routes with query parameters

--- routes.js
+++ routes.js
@@ -16,7 +16,7 @@ const routes = [
         }
     },
     {
-        method:'GET',
+        method: 'GET',
         path: '/about',
         handler: (request, h) => {
             return 'About Page'
@@ -33,8 +33,12 @@ const routes = [
         method: 'GET',
         path: '/hello/{name?}',
         handler: (request, h) => {
-           const { name = 'stranger'} = request.params
-           return `Hello, ${name}!` 
+            const { name = 'stranger' } = request.params
+            const { lang } = request.query
+            if (lang == 'id') {
+                return `Hi, ${name}!`
+            }
+            return `Hello, ${name}!`
         }
     },
     {
